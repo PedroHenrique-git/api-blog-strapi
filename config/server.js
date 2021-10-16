c70698e30592a44b91329f36deaeb75cf575b4bd
PedroHenrique-git <pedromf34@outlook.com>
2021-10-15 21:50:21 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '29e58c5cf8c2472030ef569f0dc66f89'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17a5ae17-92a4-453c-8433-d56dca3f192b'
+};
